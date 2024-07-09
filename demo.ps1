586025de6e4b0028f2e8e8b13fa48b72189f516e
Denis Gukov <fiftin@outlook.com>
2024-07-09 22:31:27 +0500

    feat: add file.md

--- demo.ps1
+++ demo.ps1
@@ -1,1 +1,42 @@
-Write-Host 'Hello, World!'
+# Define colors
+$HeaderColor = "Cyan"
+$BoldColor = "Yellow"
+$CodeColor = "Green"
+$DefaultColor = "White"
+
+# Function to print colored text
+function Write-ColoredText {
+    param (
+        [string]$Text,
+        [string]$Color
+    )
+    Write-Host $Text -ForegroundColor $Color
+}
+
+# Read the Markdown file
+$MarkdownFile = ".\file.md"
+$Content = Get-Content $MarkdownFile
+
+# Process each line
+foreach ($line in $Content) {
+    if ($line -match '^# ') {
+        # Header level 1
+        Write-ColoredText $line $HeaderColor
+    } elseif ($line -match '^## ') {
+        # Header level 2
+        Write-ColoredText $line $HeaderColor
+    } elseif ($line -match '^### ') {
+        # Header level 3
+        Write-ColoredText $line $HeaderColor
+    } elseif ($line -match '\*\*(.+?)\*\*') {
+        # Bold text
+        $boldText = $line -replace '\*\*(.+?)\*\*', "$1"
+        Write-ColoredText $boldText $BoldColor
+    } elseif ($line -match '```') {
+        # Code block
+        Write-ColoredText $line $CodeColor
+    } else {
+        # Default text
+        Write-ColoredText $line $DefaultColor
+    }
+}
---
+++ file.md
@@ -0,0 +1,7 @@
+# Hello World!
+
+1. How are you?
+
+## Test 123
+
+**HA HA HA**
